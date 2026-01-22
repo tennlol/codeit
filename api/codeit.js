95f9c9452d580971311af14b14bbd5a61a361ccd
fwiend <92182432+fwiends@users.noreply.github.com>
2026-01-22 21:19:57 +0000

    added console data logging to track errors

--- api/codeit.js
+++ api/codeit.js
@@ -32,6 +32,7 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
+    console.log(data); 
     res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
